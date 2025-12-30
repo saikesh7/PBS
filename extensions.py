593a206e08ee6f04b60a07688729800e38d9e1df
saikesh7 <saikarna07@gmail.com>
2025-12-30 10:45:37 +0530

    Delete extensions.py

--- extensions.py
+++
@@ -1,9 +0,0 @@
-# extensions.py
-from flask_pymongo import PyMongo
-from flask_mail import Mail
-from flask_bcrypt import Bcrypt
-
-# Create instances of all extensions
-mongo = PyMongo()
-mail = Mail()
-bcrypt = Bcrypt()
