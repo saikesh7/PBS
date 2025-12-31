25b55ef8a87e0c2872dc97d0c48e0e9228d26c7a
unknown <saikarna07@gmail.com>
2025-12-31 12:30:11 +0530

    recent

---
+++ create_hr_user.py
@@ -0,0 +1,34 @@
+{
+ 
+  "name": "Akhil V2 PM3",
+  "email": "akhilv2pm3@gmail.com",
+  "phone": "7793920128",
+  "employee_id": "V2PM3",
+  "password_hash": "$2b$12$lUFy8KxfZ102KqUVZITsqOAJBPgrgfJ/.jiNBWosIE7Pn6HGyw9tO",
+  "role": "Employee",
+  "is_first_login": false,
+  "grade": "C2",
+  "department": "Management",
+  "location": "India",
+  "employee_level": "Mid-Level",
+  "manager_id": null,
+  "dp_id": null,
+  "is_active": true,
+  "dashboard_access": [
+    "pm",
+    "hr",
+    "ta_up",
+    "ta_va",
+    "employee_db"
+  ],
+  "joining_date": {
+    "$date": "2020-05-29T00:00:00.000Z"
+  },
+  "exit_date": null,
+  "created_at": {
+    "$date": "2025-11-05T11:58:25.635Z"
+  },
+  "updated_at": {
+    "$date": "2025-11-05T11:58:25.635Z"
+  }
+}
